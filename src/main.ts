9c8faa67bfd4eee4b8c8e43a9dc4c3e97638ea06
censuradho <gustavoleite.me@gmail.com>
2024-11-17 21:41:47 -0300

    feature: Implement helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import 'reflect-metadata';
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import helmet from 'helmet'
 
 import { corsConfig } from './shared/config/cors'
 import { environment } from './shared/environment'
@@ -12,10 +13,12 @@ import { routes } from './infra/routes/routes'
 const app = express()
 
 app.use(loggerHttp)
+app.use(helmet())
 app.use(cookieParser())
 app.use(express.json())
 app.use(cors(corsConfig))
 app.use(routes)
+app.disable('x-powered-by')
 
 app.listen(environment.PORT, () => {
   console.log(`Listen on PORT: ${environment.PORT}/`);
