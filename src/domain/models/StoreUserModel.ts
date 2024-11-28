4966beb42c4b64b35f34c917dbae519e88f2bc49
censuradho <gustavoleite.me@gmail.com>
2024-11-28 19:07:10 -0300

    feature: add isVerified to the StoreUserModel

--- src/domain/models/StoreUserModel.ts
+++ src/domain/models/StoreUserModel.ts
@@ -5,11 +5,13 @@ export class StoreUserModel {
   lastName: string
   email: string
   id: string
+  isVerified?: boolean | null
 
   constructor (payload: StoreUserEntity) {
     this.id = payload.id
     this.firstName = payload.firstName
     this.email = payload.email
     this.lastName = payload.lastName
+    this.isVerified = payload.isVerified
   }
 }
